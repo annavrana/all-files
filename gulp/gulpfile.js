99da9dabb95b4ef782045c0a801b42a26476538d
Vrana Anna <anna.pedan@ukr.net>
2018-12-22 04:02:40 +0200

    first commit

---
+++ gulp/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs');
+
+gulp.task('sass', function () {
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    })
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload)
+});
